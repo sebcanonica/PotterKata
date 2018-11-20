c3189a3331c0bee68b92ab4fc8428216cbfc0a92
Sébastien CANONICA <sebcano.biz@laposte.net>
2018-11-20 13:03:45 +0100

    TCR autocommit

--- MyLibrary/BookStore.Tests.cs
+++ MyLibrary/BookStore.Tests.cs
@@ -12,6 +12,7 @@ namespace MyLibrary
 		[TestCase(0, TestName= "Return_0_for_an_empty_basket")]
 		[TestCase(8, Books.Book1, TestName = "Return_8_for_a_basket_with_1_book")]
 		[TestCase(16, Books.Book1, Books.Book1, TestName = "Return_16_for_a_basket_with_the_same_book_twice")]
+		[TestCase(15.2, Books.Book1, Books.Book2, TestName = "Return a 5% discount for 2 different books")]
 		public void TestBookStore(decimal totalPrice, params Books[] books)
 		{
 			var store = new BookStore(books);
--- MyLibrary/BookStore.cs
+++ MyLibrary/BookStore.cs
@@ -17,12 +17,20 @@ namespace MyLibrary
 
 		public decimal ComputePrice()
         {
-			return _books.Count * 8m;
+			var price = _books.Count * 8m;
+
+			if(_books.Count == 2 && _books[0] != _books[1])
+			{
+				price *= 0.95m;
+			}
+
+			return price;
         }
     }
 
 	public enum Books
 	{
-		Book1
+		Book1,
+		Book2
 	}
 }
