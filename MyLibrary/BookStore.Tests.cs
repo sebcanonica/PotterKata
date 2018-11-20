12813df2a9b971b97ac386fde47fc64ebc92ba7a
Sébastien CANONICA <sebcano.biz@laposte.net>
2018-11-20 13:00:08 +0100

    TCR autocommit

--- MyLibrary/BookStore.Tests.cs
+++ MyLibrary/BookStore.Tests.cs
@@ -11,19 +11,12 @@ namespace MyLibrary
     {
 		[TestCase(0, TestName= "Return_0_for_an_empty_basket")]
 		[TestCase(8, Books.Book1, TestName = "Return_8_for_a_basket_with_1_book")]
+		[TestCase(16, Books.Book1, Books.Book1, TestName = "Return_16_for_a_basket_with_the_same_book_twice")]
 		public void TestBookStore(decimal totalPrice, params Books[] books)
 		{
 			var store = new BookStore(books);
 			var actual = store.ComputePrice();
 			Check.That(actual).IsEqualTo(totalPrice);
 		}
-
-		[Test]
-		public void Return_16_for_a_basket_with_the_same_book_twice()
-		{
-			var actual = new BookStore(Books.Book1, Books.Book1).ComputePrice();
-			Check.That(actual).IsEqualTo(16m);
-		}
-
     }
 }
