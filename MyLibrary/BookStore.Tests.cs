7e1d25453871700515e8976cfce6618a97db57d4
Sébastien CANONICA <sebcano.biz@laposte.net>
2019-12-03 14:28:05 +0100

    Maximizing bundle of 4

--- MyLibrary/BookStore.Tests.cs
+++ MyLibrary/BookStore.Tests.cs
@@ -3,18 +3,56 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using static MyLibrary.Books;
 
 namespace MyLibrary
 {
     [TestFixture]
     class BookStore_should
     {
-		[TestCase(0, TestName= "Return_0_for_an_empty_basket")]
-		[TestCase(8, Books.Book1, TestName = "Return_8_for_a_basket_with_1_book")]
-		[TestCase(16, Books.Book1, Books.Book1, TestName = "Return_16_for_a_basket_with_the_same_book_twice")]
-		[TestCase(15.2, Books.Book1, Books.Book2, TestName = "Return a 5% discount for 2 different books")]
-		[TestCase(21.6, Books.Book1, Books.Book2, Books.Book3, TestName = "Return a 10% discount for 3 different books")]
-		[TestCase(23.2, Books.Book1, Books.Book2, Books.Book1, TestName = "Return a 5% discount for 3 books including 2 different books")]
+		[TestCase(0, 
+			TestName= "Return_0_for_an_empty_basket")]
+		[TestCase(8, Book1, 
+			TestName = "Return_8_for_a_basket_with_1_book")]
+		[TestCase(16, Book1, Book1, 
+			TestName = "Return_16_for_a_basket_with_the_same_book_twice")]
+		[TestCase(8 * 2 * 0.95, Book1, Book2, 
+			TestName = "Return a 5% discount for 2 different books")]
+		[TestCase(8 * 3 * 0.9, Book1, Book2, Book3, 
+			TestName = "Return a 10% discount for 3 different books")]
+		[TestCase(8 * 5 * 0.75, Book1, Book2, Book3, Book4, Book5,
+			TestName = "Return a 25% discount for 5 different books")]
+		[TestCase(23.2, Book1, Book2, Book1, 
+			TestName = "Return a 5% discount for 3 books including 2 different books")]
+		[TestCase(30.4, Book1, Book2, Book1, Book2, 
+			TestName = "Return a 5% discount for 2 bundles of 2 different books")]
+		[TestCase(8 * 4 * 0.8 + 8 * 2 * 0.95, Book1, Book1, Book2, Book3, Book3, Book4,
+			TestName = "1 bundle of 4 and 1 bundle of 2")]
+		[TestCase(8 * 5 * 0.75 + 8, Book1, Book2, Book2, Book3, Book4, Book5,
+			TestName = "1 bundle of 5 and 1 solo")]
+		[TestCase(2 * (8 * 4 * 0.8), Book1, Book1, Book2, Book2, Book3, Book3, Book4, Book5,
+			TestName = "2 bundles of 4")]
+		[TestCase(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8),
+			Book1, Book1, Book1, Book1, Book1,
+			Book2, Book2, Book2, Book2, Book2,
+			Book3, Book3, Book3, Book3,
+			Book4, Book4, Book4, Book4, Book4,
+			Book5, Book5, Book5, Book5,
+			TestName = "3 bundles of 5 and 2 bundle of 4")]
+		[TestCase(8*5*0.75 + 8*4*0.8 + 8*2*0.95 + 8,//78.8, // 1 2 2 3 4
+			Book1, Book1, Book1,
+			Book2, Book2,
+			Book3, Book3, Book3, Book3,
+			Book4, Book4,
+			Book5,
+			TestName = "1 of 5, 1 of 4, 1 of 2 and 1 solo")]
+		[TestCase( 3 * 8 * 4 * 0.8 + 8 * 2 * 0.95 + 8, //100, // 1 2 3 4 5
+			Book1,
+			Book2, Book2,
+			Book3, Book3, Book3,
+			Book4, Book4, Book4, Book4,
+			Book5, Book5, Book5, Book5, Book5,
+			TestName = "3 of 4, 2 of 2 and 1 solo")]
 		public void TestBookStore(decimal totalPrice, params Books[] books)
 		{
 			var store = new BookStore(books);
--- MyLibrary/BookStore.cs
+++ MyLibrary/BookStore.cs
@@ -15,32 +15,59 @@ namespace MyLibrary
 			_books = books.ToList();
 		}
 
-		public decimal BundlePrice(int booksInBundle)
-		{
-			decimal [] BUNDLE_PRICES = { 0m, 8m, 15.2m, 21.6m };
-			return BUNDLE_PRICES[booksInBundle];
-		}
-
 		public decimal ComputePrice()
         {
-			var distinct = _books.Distinct();
-			if (distinct.Count() == _books.Count)
-			{
-				return BundlePrice(_books.Count);
+			var bookCounts = _books.GroupBy(book => book)
+				.OrderBy(group => group.Count());
+
+			if (bookCounts.Count() == 5)
+			{				
+				var bundlesOf4 = bookCounts.Skip(1).First().Count();
+				var bundlesOf3 = bookCounts.Skip(2).First().Count();
+				if (bundlesOf3 > bundlesOf4)
+				{
+					// Maximize number of bundles of 4
+					var priceWithBundlesOf4 = 0m;
+					var bundlesOf5 = bookCounts.First().Count();
+					var additionalBundlesOf4 = Math.Min(bundlesOf5, bundlesOf3 - bundlesOf4);
+					var pricedBundlesOf5 = bundlesOf5 - additionalBundlesOf4;
+					var pricedBundlesOf4 = bundlesOf4 + additionalBundlesOf4 - pricedBundlesOf5;
+					priceWithBundlesOf4 = BundlePrice(5) * pricedBundlesOf5
+						+ BundlePrice(4) * pricedBundlesOf4
+						+ BookCountsPrice(bookCounts.Skip(2), bundlesOf4 + additionalBundlesOf4);
+					return priceWithBundlesOf4;
+				}				
 			}
-			else
-			{
-				var uniquePrice = 8m *(_books.Count - distinct.Count());
-				var bundlePrice = BundlePrice(distinct.Count());
-				return uniquePrice + bundlePrice;
-			}
+
+			return BookCountsPrice(bookCounts);
         }
-    }
+
+		private decimal BookCountsPrice(IEnumerable<IGrouping<Books,Books>> bookCounts, int booksInstanceAlreadyPriced = 0)
+		{
+			var price = 0m;
+			var maxBundleSize = bookCounts.Count();
+			foreach (var bookCount in bookCounts)
+			{
+				price += BundlePrice(maxBundleSize) * (bookCount.Count() - booksInstanceAlreadyPriced);
+				maxBundleSize -= 1;
+				booksInstanceAlreadyPriced = bookCount.Count();
+			}
+			return price;
+		}
+
+		private decimal BundlePrice(int booksInBundle)
+		{
+			decimal[] BUNDLE_PRICES = { 0m, 8m, 15.2m, 21.6m, 25.6m, 30m };
+			return BUNDLE_PRICES[booksInBundle];
+		}
+	}
 
 	public enum Books
 	{
 		Book1,
 		Book2,
-		Book3
+		Book3,
+		Book4,
+		Book5
 	}
 }
